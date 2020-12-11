281d3c52b3c5c35af3110cfda6f1506840c3b398
diksha-kalra <kalradiksha11@gmail.com>
2020-12-12 01:00:52 +0530

    Added: storing the data in list

--- src/main/java/com/cg/employeepayrollapp/services/IEmployeePayrollService.java
+++ src/main/java/com/cg/employeepayrollapp/services/IEmployeePayrollService.java
@@ -12,7 +12,7 @@ public interface IEmployeePayrollService {
 
 	EmployeePayrollData createEmployeePayrollData(EmployeePayrollDTO empPayrollDTO);
 
-	EmployeePayrollData updateEmployeePayrollData(EmployeePayrollDTO empPayrollDTO);
-
 	void deleteEmployeePayrollData(int empId);
+
+	EmployeePayrollData updateEmployeePayrollData(int empId, EmployeePayrollDTO employeeDTO);
 }
